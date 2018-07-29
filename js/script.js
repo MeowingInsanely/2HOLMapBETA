13af1335064bda23e9c79f2b4ac9cf20a188ce46
Stefnotch <brandmairstefan@gmail.com>
2018-07-29 21:53:51 +0200

    probably won't fix it either

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ cat = document.getElementById("cat-vas");
 
 // Magical bit of code to stop the canvas stretching
 cat.parentElement.style.width = "0px";
-window.getComputedStyle(cat);
+window.getComputedStyle(cat.parentElement);
 cat.width = cat.clientWidth;
 cat.height = cat.clientHeight;
 
